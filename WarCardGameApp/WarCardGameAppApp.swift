ad0b853a931fc65cfaccda15a6c478f558786446
ZachMommaerts <zmommaerts@gmail.com>
2023-07-13 13:45:08 -0500

    Initial Commit

---
+++ WarCardGameApp/WarCardGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameAppApp.swift
+//  WarCardGameApp
+//
+//  Created by Zach Mommaerts on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
